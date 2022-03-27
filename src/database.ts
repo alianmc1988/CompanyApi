7413d984069f31428f9b40325ed5dc6cda05bd4c
manolisfcb <79519842+manolisfcb@users.noreply.github.com>
2022-03-27 01:37:42 -0300

    refactoring database file

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,12 @@
-import mongoose, { ConnectionStates } from 'mongoose';
+import mongoose from 'mongoose';
 
 const URI_DB: string = process.env.URI || '';
 
 mongoose
 	.connect(URI_DB)
-	.then((db: Object) => console.log(`DB is connected `))
+	.then((db: any) => {
+		if (db.connections[0]._readyState) {
+			console.log('DB connected');
+		}
+	})
 	.catch((err) => console.log(err.message));
